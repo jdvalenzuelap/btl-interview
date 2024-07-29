1f4ab3ba3a3e65352b5dedc07badc53aafd2548f
Juan Diego Valenzuela <42612966+jdvalenzuelap@users.noreply.github.com>
2024-07-29 16:49:31 +0000

    Added cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const app = express()
-const breeds = require('./data.js')
-const port = 4000
+const express = require("express");
+const app = express();
+const breeds = require("./data.js");
+const cors = require("cors");
+const port = 4000;
 
-app.get('/breeds', (req, res) => {
-  res.json(breeds)
-})
+app.use(cors());
+
+app.get("/breeds", (req, res) => {
+  res.json(breeds);
+});
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
